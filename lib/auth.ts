7ae37f196d1eb513d1c0745fce38db5a951b5ea7
RaisonSalvador <raisonsalvador0@gmail.com>
2025-08-16 12:38:26 +0800

    fix: changed the variable name of env

--- lib/auth.ts
+++ lib/auth.ts
@@ -28,8 +28,8 @@ type Tprofile = {
 const authConfig: NextAuthConfig = {
   providers: [
     Google({
-      clientId: process.env.AUTH_GOOGLE_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
 
